b71164d96160771a82a2e73aa8852eda40d340b5
TheBolshe <bibsim91@gmail.com>
2019-11-26 14:56:35 +0100

    update

--- js/getData.js
+++ js/getData.js
@@ -1,13 +1,19 @@
-function getData () {
+function getData() {
   var fin = {};
   var prog = new XMLHttpRequest();
   prog.onreadystatechange = function() {
-    //document.getElementById("main").innerHTML = this.responseText;
+    fin.dates = this.responseText;
+
+    var data = new XMLHttpRequest();
+    data.open("POST", "data/donneeSpectacle/tartuffe.json", true);
+    data.onreadystatechange = function() {
+      fin.infos = this.responseText;
+      document.getElementById("main").innerHTML = JSON.stringify(fin);
+    }
+    data.send();
   }
   prog.open("POST", "php/filtreInfos.php", true);
   prog.send("nom='Le Tartuffe ou l'imposteur'")
-  var data = new XMLHttpRequest();
-  prog.open("POST", "data/donneeSpectacle/tartuffe.json", true);
-  prog.send();
-  var
+
+
 }
