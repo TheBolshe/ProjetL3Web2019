cbdb55cf6dee3b107f70f87c1a3e61ac41d08d62
TheBolshe <bibsim91@gmail.com>
2019-12-03 15:21:19 +0100

    Format json bien

--- js/getData.js
+++ js/getData.js
@@ -1,21 +1,29 @@
-function getData() {
-  var fin = {};
-  var callDates = $.ajax({
+function getData(arg, callback) {
+  let fin = {};
+  let callDates = $.ajax({
     type: "POST",
     url: "php/filtreInfos.php",
     data: "nom=Le Tartuffe ou l'imposteur",
     success: function(data) {
-      console.log(data);
+      //console.log(data);
       fin.dates = data;
     }
   });
-  var callInfos = $.ajax({
+  let callInfos = $.ajax({
     type: "POST",
     url: "data/donneeSpectacle/tartuffe.json",
     success: function(data) {
-      console.log(data);
+      //console.log(data);
       fin.infos = data;
     }
   });
-  $.when (callDates, callInfos).done(function() {console.log(fin)});
+  $.when(callDates, callInfos).done(function() {
+    //console.log(fin);
+    callback(fin);
+  });
+}
+
+
+function printData() {
+  console.log();
 }
