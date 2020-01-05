0567715d48c3ce5b072505b74e802e7bb2d7980f
sefirosse <sefirosse@gmail.com>
2020-01-05 18:47:59 +0100

    erreur

--- js/canvas.js
+++ js/canvas.js
@@ -30,43 +30,46 @@ function getDataStatsbis(){
   let callStats = $.ajax({
     type : "POST",
     url : "php/filtreQuandOu.php",
-    data : ,
     success : function(data){
-      ye = data.E;
-      ysj = data.SJ;
-      ysa = data.SA;
+      affiche(data)
     }
   });
 }
 
-let ctx = document.getElementById('canvas').getContext('2d');
+function affiche(donnee){
+  ye = donnee.E;
+  ysj = donnee.SJ;
+  ysa = donnee.SA;
+  
+  let ctx = document.getElementById('canvas').getContext('2d');
 
-Chart.defaults.scale.ticks.beginAtZero = true;
+  Chart.defaults.scale.ticks.beginAtZero = true;
 
-let barChart = new Chart(ctx,{
-  type : 'bar',
-  data : {
-    labels : ["Barbara, où rêvent mes saisons","Le château de ma mère","Fricassée de berniques sur lit de Prévert","Hugo es tu là?","La demande en mariage et L'ours","La gloire de mon père","La promesse de l'aube","Le marchand de Venise","Le mariage","Le prophète","Le Tartuffe ou l'imposteur","Les confessions : Les années de Jeunesse","Les confessions : Les années de Maturité","Les confessions : Les années de Sagesse","Les soliloques de Mariette","Mademoiselle Julie","Port Racines","Titre Provisoire"],
-    datasets : [
-      {
-        label : 'Tarif Enfant',
-        data : ye,
-        backgroundColor : 'blue',
-      },{
-        label : 'Tarif Jeune',
-        data : ysj,
-        backgroundColor : 'green',
-      },{
-        label : 'Tarif Adult',
-        data : ysa,
-        backgroundColor : 'yellow',
+  let barChart = new Chart(ctx,{
+    type : 'bar',
+    data : {
+      labels : ["Barbara, où rêvent mes saisons","Le château de ma mère","Fricassée de berniques sur lit de Prévert","Hugo es tu là?","La demande en mariage et L'ours","La gloire de mon père","La promesse de l'aube","Le marchand de Venise","Le mariage","Le prophète","Le Tartuffe ou l'imposteur","Les confessions : Les années de Jeunesse","Les confessions : Les années de Maturité","Les confessions : Les années de Sagesse","Les soliloques de Mariette","Mademoiselle Julie","Port Racines","Titre Provisoire"],
+      datasets : [
+        {
+          label : 'Tarif Enfant',
+          data : ye,
+          backgroundColor : 'blue',
+        },{
+          label : 'Tarif Jeune',
+          data : ysj,
+          backgroundColor : 'green',
+        },{
+          label : 'Tarif Adult',
+          data : ysa,
+          backgroundColor : 'yellow',
+        }
+      ]
+    },
+    options : {
+      scales: {
+        xAxes: [{ stacked: true }],
+        yAxes: [{ stacked: true }]
       }
-    ]
-  },
-  options : {
-    scales: {
-      xAxes: [{ stacked: true }],
-      yAxes: [{ stacked: true }]
     }
-  }
-});
+  });
+}
