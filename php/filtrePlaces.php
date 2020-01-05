7a959bb845a5b820bb66c31df14dfce6ea623b7e
TheBolshe <bibsim91@gmail.com>
2020-01-05 19:23:46 +0100

    erreur résolue

--- php/filtrePlaces.php
+++ php/filtrePlaces.php
@@ -4,12 +4,12 @@
     "SJ"  => [],
     "SA" => [],
     "E"   => []
-  )
+  );
   $csv = array_map('str_getcsv', file('../data/ResultatsFestival.csv'));
   for ($i = 1; $i < count($csv); $i++) {
-    $list[SJ][] = $csv[$i][9];
-    $list[SA][] = $csv[$i][10];
-    $list[E][] = $csv[$i][11];
+    $list["SJ"][] = $csv[$i][9];
+    $list["SA"][] = $csv[$i][10];
+    $list["E"][] = $csv[$i][11];
   }
 
   echo json_encode($list);
